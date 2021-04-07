a3e3ad10107e7258af4339497c6f123946b1776a
charlenekanda <charlenekanda@live.fr>
2021-04-07 16:59:51 +0200

    update nav bar + form position

--- main.js
+++ main.js
@@ -1,15 +1,29 @@
 window.onload = function() {
     setInterval(randomBackground, 15000);
 
-    function randomBackground() {
-        let head = document.querySelector('.head');
-
-        let images = [
-            'images/background-1.jpeg',
-            'images/background-2.jpeg',
-            'images/background.jpg',
-        ];
-
-        head.style.backgroundImage = "url(" + images[Math.floor(Math.random() * images.length)] + ")";
+    let burgerIcon = document.querySelector('#burger');
+    burgerIcon.onclick = function (){
+        if (this.classList.contains('show')) {
+            this.classList.remove("text-secondary");
+            this.classList.remove("burger-active");
+            this.classList.add('text-white');
+        } else {
+            this.classList.add('burger-active');
+            this.classList.add('text-secondary');
+            this.classList.remove("text-white");
+        }
     }
+
+};
+
+function randomBackground() {
+    let head = document.querySelector('.head');
+
+    let images = [
+        'images/background-1.jpeg',
+        'images/background-2.jpeg',
+        'images/background.jpg',
+    ];
+
+    head.style.backgroundImage = "url(" + images[Math.floor(Math.random() * images.length)] + ")";
 }
